67ca487d26af331e0029b2b4733d5b6c3fa9ef6f
Susmit Bahadkar <121481904+bobbybacala@users.noreply.github.com>
2025-02-10 13:25:05 +0530

    getting data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,63 @@
 import { useState } from 'react'
 import './App.css'
-
+import axios from 'axios'
 function App() {
+
+  // query will be the topic you want the news about
+  const [query, setQuery] = useState('')
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const API_KEY = import.meta.env.VITE_REACT_APP_NEWS_API_KEY
+
+  // testing if the api works
+  const fetchNews = async () => {
+    // if the query is empty return
+    if (!query) {
+      return
+    }
+    
+    // set loading true, ie we are fetching the news
+    setLoading(true)
+
+    console.log(query);
+    // call the api in try and catch block
+    try {
+      // clog the query inputted
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`)
+      setArticles(response.data.articles)
+    } catch (error) {
+      console.log(`Error Fetching News: ${error.message}`);
+    } finally {
+      setLoading(false)
+    }
+
+    console.log(articles);
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    fetchNews()
+  }
+  
 
   return (
     <>
+      <h1>News App</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          onChange={(e) => setQuery(e.target.value)}
+          className='border'
+          placeholder='Enter a topic or area'
+          value={query} />
+        <button
+          type='submit'
+          className='bg-sky-500 text-black'
+        >
+          Get News
+        </button>
+      </form>
     </>
   )
 }
